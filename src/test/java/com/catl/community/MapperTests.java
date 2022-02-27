55fa30104f1cb2df2242006ea6a80b57ee1423a9
hoping <stevenfok@126.com>
2022-02-27 22:31:13 +0800

    2022年2月27日提交 第二章完毕

--- src/test/java/com/catl/community/MapperTests.java
+++ src/test/java/com/catl/community/MapperTests.java
@@ -1,8 +1,10 @@
 package com.catl.community;
 
 import com.catl.community.dao.DiscussPostMapper;
+import com.catl.community.dao.LoginTicketMapper;
 import com.catl.community.dao.UserMapper;
 import com.catl.community.entity.DiscussPost;
+import com.catl.community.entity.LoginTicket;
 import com.catl.community.entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,6 +26,9 @@ public class MapperTests {
 
     @Autowired
     private DiscussPostMapper discussPostMapper;
+
+    @Autowired
+    private LoginTicketMapper loginTicketMapper;
 
     @Test
     public void testSelectUser() {
@@ -74,5 +79,26 @@ public class MapperTests {
         //int rows = discussPostMapper.selectDiscussPostRows(0);
         int rows = discussPostMapper.selectDiscussPostRows(149);
         System.out.println(rows);
+    }
+
+    @Test
+    public void testInsertLoginTicket() {
+        LoginTicket loginTicket = new LoginTicket();
+        loginTicket.setUserId(101);
+        loginTicket.setTicket("abc");
+        loginTicket.setStatus(0);
+        loginTicket.setExpired(new Date(System.currentTimeMillis() + 1000 * 60 * 10));
+
+        loginTicketMapper.insertLoginTicket(loginTicket);
+    }
+
+    @Test
+    public void testSelectLoginTicket(){
+        LoginTicket loginTicket = loginTicketMapper.selectByTicket("abc");
+        System.out.println(loginTicket);
+        
+        loginTicketMapper.updateStatus("abc",1);
+        loginTicket = loginTicketMapper.selectByTicket("abc");
+        System.out.println(loginTicket);
     }
 }
