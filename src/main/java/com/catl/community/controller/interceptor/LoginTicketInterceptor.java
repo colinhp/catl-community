48c63296d4e1e4fd63e30d297f9f8556b011256d
hoping <stevenfok@126.com>
2022-02-27 22:58:55 +0800

    2022年2月27日提交 第二章完毕

---
+++ src/main/java/com/catl/community/controller/interceptor/LoginTicketInterceptor.java
@@ -0,0 +1,57 @@
+package com.catl.community.controller.interceptor;
+
+import com.catl.community.entity.LoginTicket;
+import com.catl.community.entity.User;
+import com.catl.community.service.UserService;
+import com.catl.community.util.CookieUtil;
+import com.catl.community.util.HostHolder;
+import org.apache.catalina.Host;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+
+@Component
+public class LoginTicketInterceptor implements HandlerInterceptor {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //从cookie中获取凭证(登录才能取到)
+        String ticket = CookieUtil.getValue(request,"ticket");
+        if(ticket != null){
+            //查询凭证
+            LoginTicket loginTicket = userService.findLoginTicket(ticket);
+            //检查凭证是否有效
+            if(loginTicket != null && loginTicket.getStatus() == 0 && loginTicket.getExpired().after(new Date())){
+                //根据凭证查询用户
+                User user = userService.findUserById(loginTicket.getUserId());
+                //在本次请求中持有用户
+                hostHolder.setUsers(user);
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        User user = hostHolder.getUser();
+        if(user != null && modelAndView != null){
+            modelAndView.addObject("loginUser",user);
+        }
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        hostHolder.clear();
+    }
+}
---
+++ src/main/java/com/catl/community/util/HostHolder.java
@@ -0,0 +1,25 @@
+package com.catl.community.util;
+
+import com.catl.community.entity.User;
+import org.springframework.stereotype.Component;
+
+/**
+ * 持有用户信息，用于代替session对象
+ */
+
+@Component
+public class HostHolder {
+    private ThreadLocal<User> users = new ThreadLocal<User>();
+    public void setUsers(User user){
+        users.set(user);
+    }
+
+    public User getUser(){
+        return users.get();
+    }
+
+    public void clear(){
+        users.remove();
+    }
+
+}
